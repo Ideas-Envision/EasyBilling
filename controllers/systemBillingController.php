974674a8a9fecdcf8489f91139ad8c8fa7fadf4d
sergiosmam <sergiom_malarcon@yahoo.com>
2017-08-05 17:42:05 -0400

    Codificación Método Verhoeff
    Se realizo la codificación inicial del algoritmo de verhoeff

--- controllers/systemBillingController.php
+++ controllers/systemBillingController.php
@@ -1,11 +1,12 @@
 <?Php
 
 class systemBillingController extends IdEnController
-	{		
+	{
 		public function __construct(){
-                parent::__construct();
-			}
-			
+            
+                parent::__construct();          
+			}     
+    
 		public function index(){
                 $this->vView->visualizar('index');
 			}
@@ -27,7 +28,50 @@ class systemBillingController extends IdEnController
                     echo 'Fecha de la Transacción: '.$vDateTransactionBilling.'<br/>';
                     echo 'Monto de la Transacción: '.$vAmountBilling.'<br/>';
                     echo 'Llave de Dosificación: '.$vDosingWrenchKey.'<br/>';
+                    echo 'Verhoeff Factura: '.$this->algorithmVerhoeff(2500).'<br/>';
                 }            
-			}    
+			}
+    
+		public function algorithmVerhoeff($vNumber){
+            
+                /* INVIERTE EL NUMERO */
+                
+                /* NUMERO VERHOEFF */
+          
+                $ck = 0;
+                $vNumber = (string) $vNumber;
+                $num_length = strlen($vNumber);
+                for ($i=$num_length-1; $i>=0; $i--){
+                    $ck = $this->$vMultiplicationTable[$ck][$this->$vPermutationTable[($num_length-$i) % 8][$vNumber[$i]]];
+                }
+                return $this->$vInverseTable[$ck];
+			}
+    
+        private static $vMultiplicationTable = array(
+            array(0,1,2,3,4,5,6,7,8,9),
+            array(1,2,3,4,0,6,7,8,9,5),
+            array(2,3,4,0,1,7,8,9,5,6),
+            array(3,4,0,1,2,8,9,5,6,7),
+            array(4,0,1,2,3,9,5,6,7,8),
+            array(5,9,8,7,6,0,4,3,2,1),
+            array(6,5,9,8,7,1,0,4,3,2),
+            array(7,6,5,9,8,2,1,0,4,3),
+            array(8,7,6,5,9,3,2,1,0,4),
+            array(9,8,7,6,5,4,3,2,1,0)
+            );
+    
+        private static $vPermutationTable = array(
+            array(0,1,2,3,4,5,6,7,8,9),
+            array(1,5,7,6,2,8,3,0,9,4),
+            array(5,8,0,3,7,9,6,1,4,2),
+            array(8,9,1,6,0,4,3,5,2,7),
+            array(9,4,5,3,1,2,6,8,7,0),
+            array(4,2,8,6,5,7,3,9,0,1),
+            array(2,7,9,3,8,0,6,4,1,5),
+            array(7,0,4,6,9,1,3,2,5,8)
+            );
+    
+        private static $vInverseTable = array(0,4,3,2,1,5,6,7,8,9);    
+    
 	}
 ?>
