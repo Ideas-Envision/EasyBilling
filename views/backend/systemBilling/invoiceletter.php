60b62290af8d374e67f8a4799f0bb42ac8ba9f9a
sergiosmam <sergiom_malarcon@yahoo.com>
2017-08-30 19:36:09 -0400

    Libreria PDF
    Se agregaron los archivos necesarios de la libreria MPDF versión 6, para que el sistema pueda generar documentos PDF desde cualquier acción.

--- views/backend/systemBilling/invoiceletter.php
+++ views/backend/systemBilling/invoiceletter.php
@@ -52,7 +52,7 @@
                         </div>
                         <br/>
                         <div class="row">
-                            <div class="col-md-8">
+                            <div class="col-xs-12 col-md-12 col-lg-8">
                                 <div class="invoice-content-2 bordered">
                                     <div class="row invoice-head">
                                         <div class="col-md-4 col-xs-6">
